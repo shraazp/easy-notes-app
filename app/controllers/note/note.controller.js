3fea83793b93cb2c88c51426b473d722badc904a
shraazp <shravyap99@gmail.com>
2021-10-19 21:34:38 +0530

    added:dto and request schema

--- app/controllers/note/note.controller.js
+++ app/controllers/note/note.controller.js
@@ -10,13 +10,14 @@ const {
 // Create and Save a new Note
 exports.create = (req, res) => {
     createNewNote(req.body.title, req.body.content).then(data => {
-        res.send(data);
+        responseObject = dtoObject.noteApiSuccess;
+        responseObject.message = data;
+        res.send(responseObject);
     }).catch(err => {
-        logger.error(err.message || "Some error occurred while creating the Note.")
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
-
-        });
+        logger.error(err.message)
+        responseObject = dtoObject.noteApiFailure;
+        responseObject.message = err.message;
+        res.send(responseObject);
     });
 };
 // Retrieve and return all notes from the database.
@@ -24,10 +25,10 @@ exports.findAll = (req, res) => {
     getNotes().then(notes => {
         res.send(notes);
     }).catch(err => {
-        logger.error("error 500 while retrieving data")
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
-        });
+        logger.error(err.message)
+        responseObject = dtoObject.noteApiFailure;
+        responseObject.message = err.message;
+        res.send(responseObject);
     })
 };
 // Find a single note with a noteId
@@ -35,16 +36,17 @@ exports.findOne = (req, res) => {
     getNote(req.params.noteId, (error, resultData) => {
         logger.error("Error retrieving note with id " + req.params.noteId)
         if (error) {
-            return res.status(500).send({
-                message: "Error retrieving note with id " + req.params.noteId
-            })
+            logger.error(err.message)
+            responseObject = dtoObject.noteApiFailure;
+            responseObject.message = err.message;
+            res.send(responseObject);
         }
 
         if (err.kind === 'ObjectId') {
             logger.error("note not found with id")
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
+            responseObject = dtoObject.noteApiFindFailure;
+            responseObject.message = err.message;
+            res.send(responseObject);
         }
         res.send(resultData);
     })
@@ -56,20 +58,18 @@ exports.update = (req, res) => { // Find note and update it with the request bod
     let title = req.body.title
     let content = req.body.content
     updateNoteId(id, title, content).then(note => {
-        console.log("this")
         res.send(note);
     }).catch(err => {
-        console.log("catch" + err)
         if (err.kind === 'ObjectId') {
             logger.error("note not found with id")
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
+            responseObject = dtoObject.noteApiFindFailure;
+            responseObject.message = err.message;
+            res.send(responseObject);
         }
-        logger.error("Error updating note with id " + req.params.noteId)
-        return res.status(500).send({
-            message: "Error updating note with id " + req.params.noteId
-        });
+        logger.error("note not found with id")
+        responseObject = dtoObject.noteApiFailure;
+        responseObject.message = err.message;
+        res.send(responseObject);
     });
 };
 // Delete a note with the specified noteId in the request
@@ -79,14 +79,13 @@ exports.delete = (req, res) => {
     }).catch(err => {
         if (err.kind === 'ObjectId' || err.name === 'NotFound') {
             logger.error("Note not found with id " + req.params.noteId)
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
-            });
+            responseObject = dtoObject.noteApiFailure;
+            responseObject.message = err.message;
+            res.send(responseObject);
         }
-        logger.error("Could not delete note with id " + req.params.noteId)
-        return res.status(500).send({
-            message: "Could not delete note with id " + req.params.noteId
-
-        });
+        logger.error("note not found with id")
+        responseObject = dtoObject.noteApiFailure;
+        responseObject.message = err.message;
+        res.send(responseObject);
     });
 };
